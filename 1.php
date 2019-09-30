23dcae2c9be76d0fd3db6d428f825636208d6af0
koldointxausti <koldointxaustii@gmail.com>
2019-09-30 13:03:26 +0200

    fix 1.php

--- 1.php
+++ 1.php
@@ -1,21 +1,31 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Ejercicio 1</title>
-</head>
-<body>
-	<?php
-		$naturales = [];
-		for($i = 0 ; $i < 10 ;$i++){
-			array_push($naturales,$i);
-		}
-		$factoriales = [];
-		for ($i = 0; $i < $naturales.length; $i = $i + 1) { 
-			for ($j = 1; $j <= $naturales[$i] ; $j = $j + 1)
-				$factoriales[$i] *= $j;
-			echo $factoriales;
-		}
-	?>
-</body>
+    <head>
+        <title>ejercicio1</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            
+            $numeros = [];
+            $factoriales = [];
+            for($i = 0 ; $i < 10 ; $i++){
+                array_push($numeros, $i);
+                if($numeros[$i]==0)
+                    array_push($factoriales, 1);
+                else{
+                    $factorial = 1;
+
+                    for ($j = 1; $j <= $numeros[$i] ; $j++)
+                        $factorial *= $j;
+
+                    array_push($factoriales, $factorial);
+                }
+
+                echo '<br>'.$factoriales[$i].'<br><br>';
+
+            }              
+
+
+        ?>
+    </body>
 </html>
